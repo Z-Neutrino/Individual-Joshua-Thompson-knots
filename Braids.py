2c7b3e7b6b5825607ef513a8516b99b817a896ea
Z-Neutrino <69902408+Z-Neutrino@users.noreply.github.com>
2026-01-09 10:38:24 +0000

    Update Braids.py

--- Braids.py
+++ Braids.py
@@ -6,18 +6,38 @@ def braid_to_pd(word, n):
     k = n + 1 # next arc label to be assigned
     pd = [] # output
     
-    for x in w:
+    for x in word:
         i = abs(x) - 1 # generator left strand
         
         if x > 0: # create new pd crossing
             pd.append([p[i+1], p[i], k, k+1])
         else:
             pd.append([p[i], k, k+1, p[i+1]])
-
-        k += 2
         # update current arcs
         p[i], p[i+1] = k, k+1
+        k += 2
 
     # close the braid's pd by replacing all final arcs with initial arcs
     m = {p[j]: j + 1 for j in range(n)} # get reverse hash
     return [[m[u] if u in m else u for u in c] for c in pd]
+
+
+
+def random_word_1(n, length):
+    # generates a random braid word
+    if length <= 0:
+        return []
+    gens = list(range(-n+1,0))+list(range(1,n))
+    return [random.choice(gens) for i in range(length)]
+
+def random_word_2(n, length):
+    # generates a random braid word without consecutive inverses
+    if length <= 0:
+        return []
+    gens = list(range(-n+1,0))+list(range(1,n))
+    word = [random.choice(gens)]
+    for i in range(length-1):
+        word.append(random.choice([x for x in gens if x != -word[-1]]))
+    return word
+
+
